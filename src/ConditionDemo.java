cd210114a8fd0b8335c4bf53596c2b0f0b73422a
maling <aa6493292@gmail.com>
2021-09-10 18:24:45 +0800

    5-1 条件运算符学习，欧耶下班班

---
+++ src/ConditionDemo.java
@@ -0,0 +1,13 @@
+public class ConditionDemo {
+    public static void main(String[] args) {
+        int a=4,b=7;
+        //求a和b最大值
+        int max;
+        if(a>b){
+            max = a;
+        }else{
+            max = b;
+        }
+        System.out.println("max="+max);
+        }
+}
