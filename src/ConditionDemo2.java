980ef6bacfdd87d6287ef360cb33e24293ddb1c8
maling <aa6493292@gmail.com>
2021-09-10 11:18:22 +0800

    3-3 学习if 和else条件结果

---
+++ src/ConditionDemo2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ConditionDemo2 {
+    public static void main(String[] args) {
+    //判断一个证书是基数还是偶数，并将结果打印输出。
+        //定义一个变量存放数据
+        //int n=11;
+        //从键盘接受数据
+        System.out.println("请输入一个整数");
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        if(n%2==0){
+            System.out.println(n+"是偶数");
+        }else {
+            System.out.println(n+"是基数");
+        }
+
+    }
+}
