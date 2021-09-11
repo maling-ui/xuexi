c1eec79dc1b49bf7f7336f3904d8b7818fc8d953
maling <aa6493292@gmail.com>
2021-09-11 19:08:05 +0800

    6-1 闰年问题案例

--- src/ConditionDemo.java
+++ src/ConditionDemo.java
@@ -1,13 +1,24 @@
 public class ConditionDemo {
     public static void main(String[] args) {
-        int a=4,b=7;
+        int a=10,b=7;
         //求a和b最大值
         int max;
         if(a>b){
-            max = a;
+            max=a;
         }else{
-            max = b;
+            max=b;
         }
         System.out.println("max="+max);
+        max=a>b?a:b;
+        System.out.println("max="+max);
+        boolean b1=a>b?(3<6):(true==false);
+        System.out.println("b1="+b1);
+
+        int z=4,y=6;
+        int n= z*y+(z%2)-(z-y);
+        System.out.println(n);
         }
+
+
+
 }
