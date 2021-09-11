4269ff4add750ee951a96c92e38a6c19a4aa0a23
maling <aa6493292@gmail.com>
2021-09-11 22:44:49 +0800

    循环输出英文字母

---
+++ src/com/imooc/flow/CharDemo.java
@@ -0,0 +1,15 @@
+package com.imooc.flow;
+
+public class CharDemo {
+    public static void main(String[] args){
+        char ch='a';
+        int count=1;
+        while(ch<='z'){
+            System.out.print(ch+" ");
+            if(count%13==0)
+                System.out.println();
+            count++;
+            ch++;
+        }
+    }
+}
