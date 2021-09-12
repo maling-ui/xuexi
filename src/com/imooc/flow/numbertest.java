5417d188d7cc199ceeea5710add7ebd6cf5ed04c
maling <aa6493292@gmail.com>
2021-09-12 10:24:35 +0800

    列出每排10位數到100

---
+++ src/com/imooc/flow/numbertest.java
@@ -0,0 +1,12 @@
+package com.imooc.flow;
+
+public class numbertest {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i%10==1)
+                System.out.println();
+                System.out.print(i+" ");
+
+            }
+        }
+    }
